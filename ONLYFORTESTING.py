64ab39ed83c124236b189dd235c30bcb8ffcb7d0
mycodezone9 <vjatseslav.aktsurin@outlook.com>
2022-11-23 00:32:13 +0200

    #

--- ONLYFORTESTING.py
+++ ONLYFORTESTING.py
@@ -1,16 +1,20 @@
 from math import *
-a=input('Enter value:')
-if a.isalnum():
-	if a.isdigit():
-		c=int(a)/2
-		print(c)
-	elif a.isalpha():
-		print(a)
+a=float(input('Please enter the number for A:'))
+b=float(input('Please enter the number for B:'))
+c=float(input('Please enter the number for C:'))
+if a!=0:
+	d=b*b-4*a*c
+	if d<0:
+		print('No solutions')
+	elif d>0:
+		x1=(-b+sqrt(d))/2*a
+		x2=(-b-sqrt(d))/2*a
+		print('x1='+str(round(x1,2))+' x2='+str(round(x2,2)))
+	elif d==0:
+		x3=-b/(2*a)
+		print(str(round(x3,2)))
 else:
-	a==float
-	print(float(a)*0.7)
-
-
+	print("A can't be 0")
 
 		
 	
--- Vjatseslav_uslovnqj_operator.py
+++ Vjatseslav_uslovnqj_operator.py
@@ -103,7 +103,25 @@ else:
 
 
 
+#NR6
 
+from math import *
+a=float(input('Please enter the number for A:'))
+b=float(input('Please enter the number for B:'))
+c=float(input('Please enter the number for C:'))
+if a!=0:
+	d=b*b-4*a*c
+	if d<0:
+		print('No solutions')
+	elif d>0:
+		x1=(-b+sqrt(d))/2*a
+		x2=(-b-sqrt(d))/2*a
+		print('x1='+str(round(x1,2))+' x2='+str(round(x2,2)))
+	elif d==0:
+		x3=-b/(2*a)
+		print(str(round(x3,2)))
+else:
+	print("A can't be 0")
 
 
 
