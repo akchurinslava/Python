fbd710cf869c5a145fcc7c8b8635f56d5ad0af9a
mycodezone9 <vjatseslav.aktsurin@outlook.com>
2022-11-23 23:09:34 +0200

    #

--- ONLYFORTESTING.py
+++ ONLYFORTESTING.py
@@ -1,20 +1,13 @@
-from math import *
-a=float(input('Please enter the number for A:'))
-b=float(input('Please enter the number for B:'))
-c=float(input('Please enter the number for C:'))
-if a!=0:
-	d=b*b-4*a*c
-	if d<0:
-		print('No solutions')
-	elif d>0:
-		x1=(-b+sqrt(d))/2*a
-		x2=(-b-sqrt(d))/2*a
-		print('x1='+str(round(x1,2))+' x2='+str(round(x2,2)))
-	elif d==0:
-		x3=-b/(2*a)
-		print(str(round(x3,2)))
+a=input('Enter value:')
+if a.isalnum():
+	if a.isdigit():
+		c=int(a)/2
+		print(c)
+	elif a.isalpha():
+		print(a)
 else:
-	print("A can't be 0")
+	a==float
+	print(float(a)*0.7)
 
 		
 	
