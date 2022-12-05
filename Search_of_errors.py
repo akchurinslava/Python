e5a130c51c9bdb9125b0098826a30ad3b01ed0c3
mycodezone9 <vjatseslav.aktsurin@outlook.com>
2022-12-05 20:54:00 +0200

    #

--- Search_of_errors.py
+++ Search_of_errors.py
@@ -1,17 +1,46 @@
-a=abs(int(input('Enter a number:')))
-if a!=0 and a!=int:
-    odds=0
-    even=0
-    while a>0:
-        if a%2==0:
-            odds+=1
-        else:
-            even+=1
-        a=a//10
-elif a==0:
-    print('Nothing to do with zero')
+while True:
+    try:
+        a=abs(int(input('Enter a number:')))
+        break
+    except ValueError:
+        print('This is not integer')
+if a==0:
+    print('Nothing to do with number')
 else:
-    a==float
-    print('This is not integer')
-print('odds in number= '+str(odds))
-print('even in number= '+str(even))
+    print('Many odds and even')
+    print()
+    c=b=a
+    even=0
+    odds=0
+    while b>0:
+        if b%2==0:
+            even+=1
+        else:
+            odds+=1
+        b//=10
+    print('even:', even)
+    print('even:', odds)
+    print()
+    print('Now we inverse number')
+    print()
+    while a>0:
+        n=a%10
+        a=a//10
+        b=b*10
+        b+=n
+    print('Inversed number is:', b)
+    print()
+    print('Check hypotyse of Sirakuz')
+    print()
+    if c%2==0:
+        print(c, 'even number')
+    else:
+        print(c, 'odd number')
+    while c!=1:
+        if c%2==0:
+            c=c/2
+        else:
+            c=(3*c+1)/2
+        print(int(c), end=" ")
+    print()
+    print('Hypotyse is right')
