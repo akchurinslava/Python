11eb86836fc94415e51e44b50e0047512ad99303
mycodezone9 <vjatseslav.aktsurin@outlook.com>
2022-12-07 23:02:25 +0200

    #

--- mathematics.py
+++ mathematics.py
@@ -6,8 +6,7 @@ a=input('Please choose your level[easy, normal, hard]:')
 
 
 if a.lower() == 'easy':
-    print('So you chose a newbie level')
-    print('It is better to start than not to start at all')
+    print('So you chose a newbie level, it is better to start than not to start at all')
     input('Please press enter to continue...')
     c1=0
     c2=0
@@ -18,7 +17,24 @@ if a.lower() == 'easy':
         check=n1+n2
         if check==a1:
             c1+=1
-        else:
-            check!=a1
-            c2+=1
-            
+            if c1==2:
+                input('If you want to see result press enter')
+                print('Your result is', )
+
+
+
+
+
+
+elif a.lower() == 'normal':
+
+
+
+
+elif a.lower() == 'hard':
+
+
+
+
+else:
+	print('Please input: easy or normal or hard')
