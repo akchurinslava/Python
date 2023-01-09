411c0b6285cd79deb6fe4a931a795d8123c6cfcf
Vyacheslav Akchurin <vjatseslav.aktsurin@outlook.com>
2023-01-09 21:54:43 +0200

    #

--- def/isikukood_lists.py
+++ def/isikukood_lists.py
@@ -1,103 +1,27 @@
 from datetime import *
 from my_module import *
-while True:
-	ik=input('Isikukood:')
-	if len(ik)==11:
-		try:
-			ikl=list(ik)
-			if int(ikl[0]) in [1,2,3,4,5,6]:
-				if int(ikl[0]) in [1,2]:
-					a=1800
-				elif int(ikl[0]) in [3,4]:
-					a=1900
-				else:
-					a=2000
-				year=int(ikl[1]+ikl[2])
-				month=int(ikl[3]+ikl[4])
-				day=int(ikl[5]+ikl[6])
-				if datetime(year,month,day):
-					s=0
-					for i in range(0,10):
-						s+=(i%9+1)*int(ikl[i])
-						print(f'{i%9+1}*{int(ikl[i])}+', end="")
-					print(s)
-					s=s-(s//11)*11
-					print(s)
-				if int(ikl[0]) in [1,3,5]:
-					print('Man')
-				else:
-					int(ikl[0]) in [2,4,6]
-					print('Woman')
-				ik3=int(ik[7:10])
-				if ik3>1 and ik3<=10:
-					mcenter='Kuressaare'
-				elif ik3>11 and ik3<=19:
-					mcenter='Tartu'
-				elif ik3>21 and ik3<=220:
-					mcenter='Ida-Tallinna'
-				elif ik3>=221 and ik3<=270:
-					mcenter='Ida-Viru'
-				elif ik3>=271 and ik3<=370:
-					mcenter='Maarjamoisa'
-				elif ik3>=371 and ik3<=420:
-					mcenter='Narva'
-				elif ik3>=421 and ik3<=470:
-					mceneter='Parnu'
-				elif ik3>=471 and ik3<=490:
-					mcenter='Pelgulinna'
-				elif ik3>=491 and ik3<=520:
-					mcenter='Jarvamaa'
-				elif ik3>=521 and ik3<=570:
-					mcenter='Rakvere'
-				elif ik3>=571 and ik3<=600:
-					mcenter='Valga'
-				elif ik3>=601 and ik3<=650:
-					mcenter='Viljandi'
-				else:
-					ik3>=651 and ik3<=700
-					mcenter='Louna-Eesti'
-				print(mcenter)
-			else:
-				print('First number is wrong')
-		except:
-			print('Data is wrong, must insert numbers')
-	else:
-		print('Isikukood must be 11 symbols')
-
-
-###
-from my_module import *
-from datetime import *
-while True:
-	ik=input('Isikukood:')
-	if len(ik)==11:
-		try:
-			ikl=list(ik)
-			if int(ikl[0]) in [1,2,3,4,5,6]:
-				if int(ikl[0]) in [1,2]:
-					a=1800
-				elif int(ikl[0]) in [3,4]:
-					a=1900
-				else:
-					a=2000
-				year=int(ikl[1]+ikl[2])
-				month=int(ikl[3]+ikl[4])
-				day=int(ikl[5]+ikl[6])
-				if datetime(year,month,day):
-					answer=Control(ik)
-					if type(answer)==int:
-						ik3=int(ik[7:10])
-						medical=Medical(ik3)
-						print(medical)
-						gender=Gender(int(ik[0]))
-						print(gender)
-					else:
-						print(answer)
-
-			else:
-				print('First number is wrong')
-		except:
-			print('Data is wrong, must insert numbers')
-	else:
-		print('Isikukood must be 11 symbols')
-
+isik = input("Enter isikukood: ")
+numbers = []
+isikukoodid = []
+if len(isik) == 11:
+    try:
+        isik_list=list(isik)
+        if int(isik_list[0]) in [1,2,3,4,5,6]:
+            if time(isik):
+                print(check_of_summ(isik))
+            else:
+                print("Not correct month")
+                numbers.append(isik)
+                print(numbers)
+        else:
+            print("First number is wrong")
+            numbers.append(isik)
+            print(numbers)
+    except:
+        print("Incorrect data type, must be numbers")
+        numbers.append(isik)
+        print(numbers)
+else:
+    print("Symbols must be 11")
+    numbers.append(isik)
+    print(numbers)
